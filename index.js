6748038558e76743792b7ed3c73685d06276c6a8
MuhammadFormanAli <muhammadformaanali@gmail.com>
2023-05-03 18:06:20 +0600

    api created for recipes

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const cors = require('cors')
 
 
 const chef = require('./data/chefData.json')
+const recipes = require('./data/recipes.json')
 
 
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send( 'Life is beautiful')
 })
 
 app.get('/chef',(req, res)=>{
   res.send(chef)
 })
+
+app.get('/chef/:id',(req, res)=>{
+  const id = req.params.id;
+  const searchedRecipes = recipes.filter(n => n.chefId === id);
+  res.send(searchedRecipes)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
