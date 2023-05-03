04604e0b8a70df48db30988efabaa6e3553d051a
MuhammadFormanAli <muhammadformaanali@gmail.com>
2023-05-03 17:05:50 +0600

    api created done for chefs data

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const chef = require('./data/chefData.json')
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chef',(req, res)=>{
+  res.send(chef)
 })
 
 app.listen(port, () => {
