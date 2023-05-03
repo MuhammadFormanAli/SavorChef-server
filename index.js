1d69593a1922bc69b15004fb4e3b77b40dc3551c
MuhammadFormanAli <muhammadformaanali@gmail.com>
2023-05-03 17:58:53 +0600

    cors installed done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors')
+
 
 const chef = require('./data/chefData.json')
 
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
